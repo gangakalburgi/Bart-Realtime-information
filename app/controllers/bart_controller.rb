d63bf5915eb9ba0933113286fcb13ff115c324e7
gangakalburgi <56897430+gangakalburgi@users.noreply.github.com>
2020-03-03 17:11:26 -0800

    Bart Home page Details

--- app/controllers/bart_controller.rb
+++ app/controllers/bart_controller.rb
@@ -1,4 +1,37 @@
+require 'Faraday'
+require 'json'
+
 class BartController < ApplicationController
-  def schedules
+  def home
+  	stations
   end
+
+  def stations
+  	response = Faraday.get 'http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y'
+  	body = JSON.parse(response.body)
+  	@stations = body['root']['stations']['station'].map do |s| 
+  		{
+  			name: s['name'],
+  			abbr: s['abbr']
+  		}
+  	end.to_json
+  end
+
+  # [GET /trips?source=<STN_ABBR>&dest=<STN_ABBR>]
+  def trips
+  	src = params[:source]
+  	dest = params[:dest]
+  	response = Faraday.get "http://api.bart.gov/api/sched.aspx?cmd=depart&orig=#{src}&dest=#{dest}&date=now&key=MW9S-E7SL-26DU-VV8V&b=0&a=4&l=1&json=y"
+  	body = JSON.parse(response.body)
+  	@trips = body['root']['schedule']['request']['trip']
+  	render json: @trips
+  end
+
+  # [GET /station?source=<STN_ABBR> ]
+  def station
+    src = params[:source]
+  	response = Faraday.get "http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=#{src}&key=MW9S-E7SL-26DU-VV8V&json=y"
+  	body = JSON.parse(response.body)
+  	@station = body['root']['stations']['station']
+  end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'bart/schedules'
+  get 'bart/stations'
+  get 'bart/trips'
+  get 'bart/station'
 
-  root 'home#index'
+
+  root 'bart#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
